66be73e528c6608cf9ffd8acaf7fe8c685cb65eb
Sofia <sofia.salazar.m@utec.edu.pe>
2023-08-28 18:02:49 -0500

    new version

--- anime.py
+++ anime.py
@@ -4,10 +4,10 @@ from flask import request
 app = Flask(__name__)
 
 topAnilist = [
-    {"id": 0, "titulo": "Gintama: THE FINAL", "puntaje": 91, "tipo":  "movie", "season":  "THE FINAL", "generos": ["action", "comedy", "drama", "sci-fi"]},
-    {"id": 1, "titulo": "Gintama", "puntaje": 90, "tipo":  "TV Show", "season":  "4", "generos": ["action", "comedy", "drama", "sci-fi"]},
-    {"id": 2, "titulo": "Fruits Basket: The Final", "puntaje": 90, "tipo":  "TV Show", "season":  "3", "generos": ["comedy", "drama", "psychological", "romance"]},
-    {"id": 3, "titulo": "Hagane no Renkinjutsushi: FULLMETAL ALCHEMIST", "puntaje": 90, "tipo":  "TV Show", "season":  "1", "generos": ["action", "adventure", "drama", "fantasy"]}
+    {'id': 0, 'titulo': 'Gintama: THE FINAL', 'puntaje': 91, 'tipo':  'movie', 'season':  'THE FINAL', 'generos': ["action", "comedy", "drama", "sci-fi"]},
+    {'id': 1, 'titulo': 'Gintama', 'puntaje': 90, 'tipo':  'TV Show', 'season':  '4', 'generos': ["action", "comedy", "drama", "sci-fi"]},
+    {'id': 2, 'titulo': 'Fruits Basket: The Final', 'puntaje': 90, 'tipo':  'TV Show', 'season':  '3', 'generos': ["comedy", "drama", "psychological", "romance"]},
+    {'id': 3, 'titulo': 'Hagane no Renkinjutsushi: FULLMETAL ALCHEMIST', 'puntaje': 90, 'tipo':  'TV Show', 'season':  '1', 'generos"': ["action", "adventure", "drama", "fantasy"]}
   ]
 
 #method get
@@ -19,60 +19,53 @@ def getList():
 @app.route('/anime/<int:id>', methods=['GET'])
 def getAnime(id):
     for anime in topAnilist:
-       if (anime['id'] == id):
+       if anime['id'] == id:
           return anime
-       else:
-          return "404. Anime no encontrado."
+    return 'Page not found', 404
 
 #method post
 @app.route('/anime', methods=['POST'])
 def addAnime():
-    new_anime = {'id': request.json.get('id'),
-                 'titulo': request.json.get('titulo'),
-                 'puntaje': request.json.get('puntaje'),
-                 'tipo': request.json.get('tipo'),
-                 'season': request.json.get('season'),
-                 'generos': request.json.get('generos')
-                 }
+    newAnime = request.get_json()
     for anime in topAnilist:
-        if(new_anime['id']==anime['id']):
-            return '400.'
-            break
-    topAnilist.append(new_anime)
-    return new_anime
+        if newAnime['id'] == anime['id']:
+            return "You can't do this action", 202
+    topAnilist.append(newAnime)
+    return topAnilist
 
 #method put
 @app.route('/anime/<int:id>', methods=['PUT'])
-def changeAnime(id):
+def putAnime(id):
     for anime in topAnilist:
-        if(anime['id'] == id):
-            anime['id'] = request.json.get('id')
-            anime['titulo'] = request.json.get('titulo')
-            anime['puntaje'] = request.json.get('puntaje')
-            anime['tipo'] =  request.json.get('tipo')
-            anime['season'] =  request.json.get('season')
-            anime['generos'] = request.json.get('generos')
-            return anime
-    return "202"
-
+        copy = request.get_json()
+        if anime['id'] == id:
+          i = 1
+          for another in topAnilist:
+             if another['id'] == copy['id']:
+                i += 1
+          if i == 2:
+             return "You can't do this action", 202
+          else:
+             anime = copy
+             return anime
 #method patch
 @app.route('/anime/<int:id>', methods=['PATCH'])
 def partialChange(id):
     change = request.get_json()
     for anime in topAnilist:
-        if(anime["id"] == id):
+        if anime["id"] == id:
             anime.update(change)
             return anime
-    return "404. No existe el anime."
+    return "Page not found", 404
 
 #method delete
 @app.route('/anime/<int:id>', methods=['DELETE'])
 def deleteAnime(id):
     for anime in topAnilist:
-        if(anime["id"] == id):
+        if anime["id"] == id:
             topAnilist.remove(anime)
             return topAnilist
-    return "404. No existe el anime."
+    return "Page not found", 404
 
 if __name__ == '__main__':
    app.run(debug = True)
